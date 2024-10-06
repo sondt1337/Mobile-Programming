3a34914493704be72dd16bfcb0ffbcdd523daea9
Son Dinh Thai <sondinh99999@gmail.com>
2024-10-06 20:42:59 +0700

    update LinearLayout

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,11 +9,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // Kích hoạt chế độ full-screen
 
-        // Thay đổi layout thành calc.xml thay vì activity_main.xml
+        // Thiết lập layout từ calc.xml
         setContentView(R.layout.calc)
 
+        // Áp dụng lắng nghe cho WindowInsets để điều chỉnh khoảng đệm (padding) khi hệ thống thanh điều hướng hiển thị
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
